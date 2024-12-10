1d57e5b195be9210a5b16849432d8b9a738336d7
TuyenH <thtuyen199@gmail.com>
2024-12-10 13:37:43 +0000

    add comment and modify syntax for exit

--- markdown2html.py
+++ markdown2html.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+''' Write a script markdown2html.py that takes an argument 2 strings:
+
+    First argument is the name of the Markdown file
+    Second argument is the output file name
+'''
+
 import sys
 import os
 
@@ -6,13 +12,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
     markdown_file = sys.argv[1]
     output_file = sys.argv[2]
 
     if not os.path.exists(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
-    sys.exit(0)
+    exit(0)
